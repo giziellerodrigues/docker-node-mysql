9cf8d12cdc78e2ee06814de5c95fc483d6910191
Gizielle Rodrigues <giziellecinttia@gmail.com>
2021-04-06 14:34:49 -0300

    Configuração proxy reverso nginx

--- node/index.js
+++ node/index.js
@@ -1,24 +1,30 @@
 const express = require('express')
 const app = express()
-const port = 3000
-// const config = {
-//     host: 'db',
-//     user: 'root',
-//     password: 'root',
-//     database:'nodedb'
-// };
-// const mysql = require('mysql')
-// const connection = mysql.createConnection(config)
+const PORT = 3000
+const APPLICATION = 'app';
+const config = {
+    host: 'db',
+    user: 'root',
+    password: 'root',
+    database:'nodedb'
+};
+const mysql = require('mysql')
+const connection = mysql.createConnection(config)
+let pessoas = ''
 
-// const sql = `INSERT INTO people(name) values('Wesley')`
-// connection.query(sql)
-// connection.end()
-
+connection.query(`SELECT name FROM people`, null,function (err, result, fields) {
+    if (err) throw err;
+    result.forEach(it => {
+         pessoas = pessoas.concat("<h1>" + it.name + "</h1>");
+    })
+})
+connection.end()
 
 app.get('/', (req,res) => {
-    res.send('<h1>Full Cycle</h1>')
+    let hmtl = '<h1>Full Cycle Rocks!</h1>' + pessoas
+    res.send(hmtl)
 })
 
-app.listen(port, ()=> {
-    console.log('Rodando na porta ' + port)
+app.listen(PORT, () =>{
+    console.log(`LISTEN ${APPLICATION} PORT 3000`)
 })
